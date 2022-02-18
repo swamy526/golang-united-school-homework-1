8710521056550d35e09b3f1b1ee778814d7ac5fa
Swamy Kurakula <swamy_kurakula@epam.com>
2022-02-18 22:41:11 +0530

    fixed two packages issue

--- solution.go
+++ solution.go
@@ -1,5 +1,15 @@
 package solution
 
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji"
+)
+
 func GetMessage() string {
 	return "Hello :world_map:"
+}
+
+func main() {
+	fmt.Println(emoji.Sprint(GetMessage()))
 }
