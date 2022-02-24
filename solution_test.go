1521ec7d24bef31f009bfd536b474f94de4e2a91
Swamy Kurakula <swamy_kurakula@epam.com>
2022-02-24 11:44:25 +0530

    fixed test cases

--- solution_test.go
+++ solution_test.go
@@ -2,8 +2,12 @@ package solution
 
 import "testing"
 
+// TestGetMessage calls solution.GetMessage, checking
+// for a valid return value.
 func TestGetMessage(t *testing.T) {
-	if GetMessage() != "Hello :world_map:" {
+	actual_msg := "Hello :world_map:"
+	msg :=  GetMessage()
+	if msg != actual_msg {
 		t.Error("Expected: Hello :world_map:")
 	}
 }
